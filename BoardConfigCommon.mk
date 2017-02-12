b2ecfd2db4a5a752ea70085db34d885d6561d8ed
tomascus <arbiter1000@gmail.com>
2017-02-12 12:12:57 +1100

    shinano-common: build: Enable HAL1 camera hacks
    Change-Id: I5a831ff94bcd4db79098ad9a2d05d053acc95f52

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -44,6 +44,7 @@ BOARD_BLUEDROID_VENDOR_CONF := device/sony/shinano-common/bluetooth/vnd_shinano.
 # Camera
 USE_DEVICE_SPECIFIC_CAMERA := true
 TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS := true
+TARGET_HAS_LEGACY_CAMERA_HAL1 := true
 
 # CM Hardware
 BOARD_HARDWARE_CLASS += device/sony/shinano-common/cmhw
