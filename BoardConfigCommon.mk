50cba8f39896a21a96a2cbbd83780dbba97706bf
tunturn <arbiter1000@gmail.com>
2017-02-22 14:12:36 +1100

    BoardConfigCommon: move other camera flags to msm8974-common

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -43,10 +43,6 @@ BOARD_HAVE_BLUETOOTH_BCM := true
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/sony/shinano-common/bluetooth
 BOARD_BLUEDROID_VENDOR_CONF := device/sony/shinano-common/bluetooth/vnd_shinano.txt
 
-# Camera
-USE_DEVICE_SPECIFIC_CAMERA := true
-TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS := true
-
 # CM Hardware
 BOARD_HARDWARE_CLASS += device/sony/shinano-common/cmhw
 
