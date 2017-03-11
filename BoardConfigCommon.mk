0fb1a17dca83816d614ecc4bcf466b559c8e9d09
Max Weffers <rcstar6696@gmail.com>
2017-03-11 08:53:23 +0100

    Add Shinano Product_Platform

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -19,6 +19,7 @@ TARGET_SPECIFIC_HEADER_PATH += device/sony/shinano-common/include
 
 # Platform
 BOARD_VENDOR_PLATFORM := shinano
+PRODUCT_PLATFORM:= shinano
 
 # Kernel information
 BOARD_KERNEL_BASE     := 0x00000000
