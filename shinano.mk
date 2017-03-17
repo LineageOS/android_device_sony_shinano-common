13aca72d33f140e4b2de152d460d2b062dab9a58
nailyk-fr <nailyk_git@nailyk.fr>
2017-03-17 14:50:26 +0100

    shinano-common: ril: Add /etc/data into device tree
    Change-Id: I6832b287ae28e634b68abdcc31970e4520203291

--- shinano.mk
+++ shinano.mk
@@ -70,6 +70,12 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += \
     tfa9890_amp
+    
+# RIL
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/netmgr_config.xml:system/etc/data/netmgr_config.xml \
+    $(LOCAL_PATH)/configs/dsi_config.xml:system/etc/data/dsi_config.xml \
+    $(LOCAL_PATH)/configs/qmi_config.xml:system/etc/data/qmi_config.xml
 
 # Bluetooth
 PRODUCT_COPY_FILES += \
