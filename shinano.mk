104f2313fa8cbeb325dded7c313ae522587bb777
xkeita <keita_nya@live.jp>
2017-06-05 04:33:28 +0000

    shinano-common: prop: Update shinano-common props
    - Removed props that are shared with msm8974-common
    - Removed deprecated/unused props
    - Added qcom cne prop for broadcom wifi board
    
    Change-Id: I8c338833456604e59d66f4a1ead6600fe5139f35

--- shinano.mk
+++ shinano.mk
@@ -86,10 +86,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/bluetooth/bt_vendor.conf:system/etc/bluetooth/bt_vendor.conf
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    bluetooth.hfp.client=1 \
-    ro.bluetooth.alwaysbleon=true
-
 # Filesystem management tools
 PRODUCT_PACKAGES += \
     e2fsck
@@ -151,84 +147,12 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     charger_res_images
 
-# Audio offload
-PRODUCT_PROPERTY_OVERRIDES += \
-    audio.offload.buffer.size.kb=32 \
-    audio.offload.gapless.enabled=false \
-    audio.offload.multiple.enabled=false \
-    audio.offload.pcm.enable=enable \
-    av.offload.enable=enable \
-    av.streaming.offload.enable=enable \
-    audio.deep_buffer.media=true
-
-# Enable AAC 5.1 output
-PRODUCT_PROPERTY_OVERRIDES += \
-    media.aac_51_output_enabled=true
-
-# Voice Call and Audio Recording
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.qc.sdk.audio.fluencetype=none \
-    persist.audio.dualmic.config=endfire \
-    persist.audio.fluence.voicecall=true \
-    persist.audio.fluence.voicerec=false \
-    persist.audio.fluence.audiorec=true \
-    persist.audio.fluence.speaker=true \
-    persist.audio.handset.mic.type=analog
-
-# aDSP
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.qualcomm.sensors.qmd=false \
-    debug.qualcomm.sns.hal=w \
-    ro.qc.sdk.sensors.gestures=false \
-    ro.qc.sensors.max_accel_rate=false \
-    ro.qc.sensors.max_gyro_rate=false \
-    ro.qc.sensors.max_mag_rate=false \
-    ro.qc.sensors.smgr_mag_cal_en=true \
-    ro.qualcomm.sensors.pedometer=false \
-    ro.qualcomm.sensors.pam=false \
-    ro.qualcomm.sensors.scrn_ortn=false \
-    ro.qualcomm.sensors.smd=sony \
-    ro.qc.sensors.step_detector=true \
-    ro.qc.sensors.step_counter=true \
-    ro.qualcomm.sensors.georv=true \
-    ro.qc.sensors.max_geomag_rotvec=50
-
 # Sensor file
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/rootdir/system/etc/init.qcom-sensor.sh:system/etc/init.qcom-sensor.sh 
 
-# GPS
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.gps.qc_nlp_in_use=1 \
-    persist.loc.nlp_name=com.qualcomm.location \
-    ro.gps.agps_provider=1 \
-    ro.qc.sdk.izat.premium_enabled=1 \
-    ro.qc.sdk.izat.service_mask=0x0
-
-# HDMI
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.demo.hdmirotationlock=false \
-    ro.hdmi.enable=true
-
-# Time
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.timed.enable=true
-
-# Touchscreen
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.input.noresample=1
-
-# WFD
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.debug.wfd.enable=1 \
-    persist.sys.wfd.virtual=0
-
 # BCM Wifi
 $(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/config/config-bcm.mk)
-
-# HWUI memory limits
-# TODO: Add it into device: 
-#$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
 
 # Include non-opensource parts
 $(call inherit-product, vendor/sony/shinano-common/shinano-common-vendor.mk)
