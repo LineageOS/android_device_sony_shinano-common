5a1542b08319596e26a6eed400a89632866fb2f5
Keita Espinoza <clouddarkgenesis@gmail.com>
2017-07-21 02:42:24 +0000

    shinano: Reestructure device init scripts
    Restructured init in fragments.
    
    - init.qcom.rc contains generic android system configs and sensors init
    - init.sony.rc contains Credential Manager inits
    - init.sony-msm8974.rc has init procedures common to the board
    - init.sony-platform.rc contains specific init values for shinano 
      devices
    - Removed sensor inits file since it's done directly by init.qcom.rc now 
    
    Change-Id: Ia5d124a6fe039eba383cb0e09b1bfaea2f99093d

--- shinano.mk
+++ shinano.mk
@@ -40,12 +40,13 @@ PRODUCT_COPY_FILES += \
 # Init
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/rootdir/fstab.qcom:root/fstab.qcom \
+    $(COMMON_PATH)/rootdir/init.shims.rc:root/init.shims.rc \
     $(COMMON_PATH)/rootdir/init.qcom.rc:root/init.qcom.rc \
-    $(COMMON_PATH)/rootdir/init.camera.rc:root/init.camera.rc \
-    $(COMMON_PATH)/rootdir/init.platform-common.rc:root/init.platform-common.rc \
-    $(COMMON_PATH)/rootdir/init.sony.rc:root/init.sony.rc \
     $(COMMON_PATH)/rootdir/init.qcom.power.rc:root/init.qcom.power.rc \
     $(COMMON_PATH)/rootdir/init.recovery.qcom.rc:root/init.recovery.qcom.rc \
+    $(COMMON_PATH)/rootdir/init.sony.rc:root/init.sony.rc \
+    $(COMMON_PATH)/rootdir/init.sony-msm8974.rc:root/init.sony-msm8974.rc \
+    $(COMMON_PATH)/rootdir/init.sony-platform.rc:root/init.sony-platform.rc \
     $(COMMON_PATH)/rootdir/init.sony.usb.rc:root/init.sony.usb.rc \
     $(COMMON_PATH)/rootdir/ueventd.qcom.rc:root/ueventd.qcom.rc
 
@@ -57,9 +58,6 @@ PRODUCT_PACKAGES += \
 	libshims_signal \
 	libshims_idd \
         libsonycamera
-
-PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/rootdir/system/bin/credmgrfirstboot.sh:system/bin/credmgrfirstboot.sh
 
 # ANT+
 PRODUCT_PACKAGES += \
