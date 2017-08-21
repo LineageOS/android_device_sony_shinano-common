6bdb7f5aa759393c719acde800f6b45a7f687bda
Nikhil Punathil <nikhilpe@gmail.com>
2017-08-22 01:07:52 +0530

    shinano-common: move some more common shims to msm8974-common
    Change-Id: I7fc609323254b5e164440444293fa4f23aad929b
    Signed-off-by: Nikhil Punathil <nikhilpe@gmail.com>

--- shinano.mk
+++ shinano.mk
@@ -52,12 +52,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/rootdir/init.qcom.power.rc:root/init.recovery.qcom.rc
 
-# Camera (stock .575 blobs)
-PRODUCT_PACKAGES += \
-	libshims_signal \
-	libshims_idd \
-        libsonycamera
-
+# Camera
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/rootdir/sbin/tad_static:root/sbin/tad_static
 
