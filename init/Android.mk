ffa8b6042d606e1a19aab318ebff30cb9d44fbff
Max Weffers <rcstar6696@gmail.com>
2017-08-25 14:00:04 +0200

    shinano: init: Add libbase as LOCAL_STATIC_LIBRARIE
    Change-Id: Iff005484769df6cf8d5618c2149396c409ca53e9

--- init/Android.mk
+++ init/Android.mk
@@ -6,6 +6,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES := system/core/init
 LOCAL_CFLAGS := -Wall
+LOCAL_STATIC_LIBRARIES := libbase
 LOCAL_SRC_FILES := init_shinano.cpp
 LOCAL_MODULE := libinit_shinano
 include $(BUILD_STATIC_LIBRARY)
