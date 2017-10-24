4cfb5bbbf3c5aee4891496512d9dc19eef799923
Keita <keita_nya@live.jp>
2017-10-24 01:58:14 -0300

    shinano-common: Implement dumpstate as a service
    Change-Id: I77f72b9541b8adcc152fe64e45fd4528cff60f4f

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -65,9 +65,6 @@ TARGET_USES_64_BIT_BINDER := true
 # Camera
 TARGET_USES_MEDIA_EXTENSIONS := true
 
-# Dumpstate
-BOARD_LIB_DUMPSTATE := libdumpstate.sony
-
 # GPS
 TARGET_PROVIDES_GPS_LOC_API := true
 
--- dumpstate/Android.mk
+++ dumpstate/Android.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2011 The Android Open Source Project
+# Copyright (C) 2016 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,15 +12,25 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-LOCAL_PATH:= $(call my-dir)
+LOCAL_PATH := $(call my-dir)
+
 include $(CLEAR_VARS)
-
-LOCAL_C_INCLUDES := frameworks/native/cmds/dumpstate
-
-LOCAL_SRC_FILES := dumpstate.c
-
-LOCAL_MODULE := libdumpstate.sony
-
+LOCAL_MODULE := android.hardware.dumpstate@1.0-service.sony
+LOCAL_INIT_RC := android.hardware.dumpstate@1.0-service.sony.rc
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_SRC_FILES := \
+    DumpstateDevice.cpp \
+    service.cpp
+LOCAL_SHARED_LIBRARIES := \
+    android.hardware.dumpstate@1.0 \
+    libbase \
+    libcutils \
+    libdumpstateutil \
+    libhidlbase \
+    libhidltransport \
+    libhwbinder \
+    liblog \
+    libutils
 LOCAL_MODULE_TAGS := optional
-
-include $(BUILD_STATIC_LIBRARY)
+LOCAL_PROPRIETARY_MODULE := true
+include $(BUILD_EXECUTABLE)
