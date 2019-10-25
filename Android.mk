547506327d0bdef05e3a3c25cf11332efeb785b9
Arian <arian.kulmer@web.de>
2019-10-25 22:29:36 +0200

    shinano-common: Beatify WCD9320 Symlink creation
    Change-Id: I0355db4289511f3008a29cc03c772cb94d50f2dd

--- Android.mk
+++ Android.mk
@@ -1,13 +1,35 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(BOARD_VENDOR_PLATFORM),shinano)
-    include $(call all-subdir-makefiles,$(LOCAL_PATH))
 
-    $(shell mkdir -p $(TARGET_OUT_ETC)/firmware/wcd9320; \
-        ln -sf /data/misc/audio/wcd9320_anc.bin \
-        $(TARGET_OUT_ETC)/firmware/wcd9320/wcd9320_anc.bin; \
-        ln -sf /data/misc/audio/wcd9320_mad_audio.bin \
-        $(TARGET_OUT_ETC)/firmware/wcd9320/wcd9320_mad_audio.bin; \
-        ln -sf /data/misc/audio/mbhc.bin \
-        $(TARGET_OUT_ETC)/firmware/wcd9320/wcd9320_mbhc.bin)
+include $(call all-subdir-makefiles,$(LOCAL_PATH))
+
+include $(CLEAR_VARS)
+
+WCD9320_BIN := wcd9320_anc.bin wcd9320_mad_audio.bin mbhc.bin
+WCD9320_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/wcd9320/,$(notdir $(WCD9320_BIN)))
+$(WCD9320_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "WCD9320 bin link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /data/misc/audio/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(WCD9320_SYMLINKS)
+
 endif
